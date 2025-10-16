714580dd6c7eb945aa087aa75c5804454b7e6555
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-16 14:44:42 -0300

    Ex3

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,15 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex1
-{
-    internal class Program
     {
-        static void Main(string[] args)
+        internal class Program
         {
+            static void Main(string[] args)
+            {
+            /*01. Considere uma matriz de n x n elementos, onde n será informado pelo usuário. Preencha a
+            matriz (os valores podem ser lidos do teclado ou gerados automaticamente). Calcule e imprima:
+            a) A soma de cada linha;
+            b) A soma de cada coluna;
+            c) A soma dos elementos da diagonal principal da matriz;
+            d) A soma dos elementos da diagonal secundária.*/
+
+                int N;
+                Console.WriteLine("Informe o número de elementos: ");
+                N = int.Parse(Console.ReadLine());
+                Random elementos = new Random();
+
+                int[,] matriz = new int[N, N];
+                for (int linha = 0; linha < N; linha++)
+                {
+                    for (int coluna = 0; coluna < N; coluna++)
+                    {
+                        matriz[linha, coluna] = elementos.Next(50);
+                        Console.Write(matriz[linha, coluna] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+
+                for (int linha = 0; linha < N; linha++)
+                {
+                    int soma = 0;
+
+                    for (int coluna = 0; coluna < N; coluna++)
+                    {
+                        // soma das linhas
+                        soma += matriz[linha, coluna];
+                    }
+                    Console.WriteLine($"Soma da linha {linha}: {soma}");
+                }
+
+                Console.WriteLine();
+
+                for (int coluna = 0; coluna < N; coluna++)
+                {
+                    int somaColuna = 0;
+
+                    for (int linha = 0; linha < N; linha++)
+                    {
+                        // soma das colunas
+                        somaColuna += matriz[linha, coluna];
+                    }
+                    Console.WriteLine($"Soma da coluna {coluna}: {somaColuna}");
+                }
+
+                int somaDiagonal = 0;
+
+                for (int linha = 0; linha < N; linha++)
+                {
+
+                    for (int coluna = 0; coluna < N; coluna++)
+                    {
+                        // soma da diagonal
+                        if (linha == coluna)
+                        {
+                            somaDiagonal += matriz[linha, coluna];
+                        }
+                    }
+                }
+                Console.WriteLine($"\n\nSoma da diagonal: {somaDiagonal}");
+
+                somaDiagonal = 0;
+
+
+                for (int coluna = N - 1, linha = 0; coluna >= 0; linha++, coluna--)
+                {
+
+                    // soma da diagonal secundária
+                    somaDiagonal += matriz[linha, coluna];
+
+                }
+                Console.WriteLine($"\n\nSoma da diagonal secundária: {somaDiagonal}\n\n");
+            }
         }
     }
-}
+
+    
--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex3
 {
@@ -10,6 +6,49 @@ namespace Ex3
     {
         static void Main(string[] args)
         {
+            /*03. Desenvolva um programa que:
+            ● Preencha uma matriz 10x10 (os valores podem ser lidos do teclado ou gerados
+            automaticamente)
+            ● Imprima a matriz gerada;
+            ● Modifique os elementos que estão na diagonal principal da matriz: cada posição da diagonal
+            principal deve receber a raiz quadrada do valor que consta naquela posição;
+            ● Imprima a matriz modificada.*/
+
+            int [,] matriz = new int [10, 10];
+            double [,] raiz = new double [10, 10];
+            Random elementos = new Random();
+
+            for (int linha = 0; linha < 10 ; linha++)
+            {
+                for (int coluna = 0; coluna < 10; coluna++)
+                {
+                    matriz[linha, coluna] = elementos.Next(50);
+                    Console.Write(matriz[linha, coluna] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n\nMatriz modificada:\n\n");
+
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna < 10; coluna++)
+                {
+                    if (linha == coluna)
+                    {
+                        raiz[linha, coluna] = Math.Sqrt(matriz[linha, coluna]);
+                        Console.Write(raiz[linha, coluna].ToString("N2") + "\t");
+                    }
+
+                    else
+                    {
+                         raiz[linha, coluna] = matriz[linha, coluna];
+                        Console.Write(raiz[linha, coluna] + "\t");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
