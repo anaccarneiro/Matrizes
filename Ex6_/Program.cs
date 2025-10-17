703d540b216c3b83dc4320febe97ca495785965c
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-17 20:16:16 -0300

    Ex 7

--- Ex6_/Program.cs
+++ Ex6_/Program.cs
@@ -1,15 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Ex6_
+namespace Ex6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*06. Elabore um programa que leia uma matriz 4 x 4 e indique se é uma matriz triangular superior.
+            Matriz triangular superior é uma matriz onde todos os elementos abaixo da diagonal principal
+            são iguais a zero.*/
+
+            int[,] matriz = new int[4, 4];
+
+            Console.WriteLine("Preencha a matriz:");
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    Console.WriteLine($"Linha {linhas}, Coluna {colunas}");
+                    matriz[linhas, colunas] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    Console.Write(matriz[linhas, colunas] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            bool zeroColuna1 = true;
+            bool zeroColuna2 = true;
+            bool zeroColuna3 = true;
+
+            for (int linhas = 1; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < 1; colunas++)
+                {
+                    if (matriz[linhas, colunas] != 0)
+                    {
+                        zeroColuna1 = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int linhas = 2; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < 2; colunas++)
+                {
+                    if (matriz[linhas, colunas] != 0)
+                    {
+                        zeroColuna2 = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int linhas = 3; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < 3; colunas++)
+                {
+                    if (matriz[linhas, colunas] != 0)
+                    {
+                        zeroColuna3 = false;
+                        break;
+                    }
+                }
+            }
+
+            if (zeroColuna1 && zeroColuna2 && zeroColuna3)
+                Console.WriteLine("\nÉ uma matriz triângular superior");
+            else
+                Console.WriteLine("\nNão é uma matriz triângular superior");
         }
     }
 }
---
+++ Ex7/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*07. Escreva um programa que leia dois valores m e n, corresponde ao número de linhas e colunas
+            de uma matriz de inteiros. Preencha a matriz (os valores podem ser lidos do teclado ou gerados
+            automaticamente). Em seguida calcule e imprima o número de linhas e o número de colunas nulas
+            da matriz (todos os caracteres 0).*/
+
+            int M = 0, N = 0;
+
+            Console.WriteLine("Número de linhas:");
+            M = int.Parse(Console.ReadLine());
+            Console.WriteLine("Número de colunas:");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[M, N];
+
+            Random elementos = new Random();
+
+            for (int linhas = 0; linhas < M; linhas++)
+            {
+                for (int colunas = 0; colunas < N; colunas++)
+                {
+                    matriz[linhas, colunas] = elementos.Next(2);
+                    Console.Write(matriz[linhas, colunas] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            bool diferenteZero = false;
+            int contLinhas = 0;
+
+            for (int linhas = 0; linhas < M; linhas++)
+            {
+                diferenteZero = false;
+
+                for (int colunas = 0; colunas < N; colunas++)
+                {
+                    if (matriz[linhas, colunas] != 0)
+                    {
+                        diferenteZero = true;
+                        break;
+                    }
+                }
+                if (!diferenteZero)
+                {
+                    contLinhas++;
+                }
+            }
+
+            int contColunas = 0;
+
+            for (int colunas = 0; colunas < N; colunas++)
+            {
+
+
+                for (int linhas = 0; linhas < M; linhas++)
+                {
+                    if (matriz[linhas, colunas] != 0)
+                    {
+                        diferenteZero = true;
+                        break;
+                    }
+                }
+                if (!diferenteZero)
+                {
+                    contColunas++;
+                }
+            }
+            Console.WriteLine($"Há {contLinhas} linhas nulas e {contColunas} colunas nulas!");
+        }
+    }
+}
