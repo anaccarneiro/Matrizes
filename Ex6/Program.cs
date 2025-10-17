e0770e1e395ad71e9d9e5aa49afac0ce8a724627
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-17 17:12:36 -0300

    Ex 5 - Corrigido

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex6
 {
@@ -10,6 +6,77 @@ namespace Ex6
     {
         static void Main(string[] args)
         {
+            /*06. Elabore um programa que leia uma matriz 4 x 4 e indique se é uma matriz triangular superior.
+            Matriz triangular superior é uma matriz onde todos os elementos abaixo da diagonal principal
+            são iguais a zero.*/
+
+            int[,] matriz = new int[4, 4];
+
+            Console.WriteLine("Preencha a matriz:");
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    Console.WriteLine($"Linha {linhas}, Coluna {colunas}");
+                    matriz[linhas, colunas] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    Console.Write(matriz[linhas, colunas] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int[] posicaoColuna1 = new int[4];
+            int[] posicaoColuna2 = new int[4];
+            int[] posicaoColuna3 = new int[4];
+            bool zeroColuna1 = true;
+            bool zeroColuna2 = true;
+            bool zeroColuna3 = true;
+
+            for (int linhas = 1; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < 1; colunas++)
+                {
+                    posicaoColuna1[linhas] = matriz[linhas, colunas];
+
+                    if (posicaoColuna1[linhas] != 0)
+                        zeroColuna1 = false;
+                }
+            }
+
+            for (int linhas = 2; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < 2; colunas++)
+                {
+                    posicaoColuna2[linhas] = matriz[linhas, colunas];
+
+                    if (posicaoColuna2[linhas] != 0)
+                        zeroColuna2 = false;
+                }
+            }
+
+            for (int linhas = 3; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < 3; colunas++)
+                {
+                    posicaoColuna3[linhas] = matriz[linhas, colunas];
+
+                    if (posicaoColuna3[linhas] != 0)
+                        zeroColuna3 = false;
+                }
+            }
+
+            if (zeroColuna1 && zeroColuna2 && zeroColuna3)
+                Console.WriteLine("\nÉ uma matriz triângular superior");
+            else
+                Console.WriteLine("\nNão é uma matriz triângular superior");
         }
     }
 }
