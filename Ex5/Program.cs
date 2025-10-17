0c27992691711015afec07b309977b98d7eb3cae
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-17 15:47:40 -0300

    Ex 5

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Xml.Linq;
 
 namespace Ex5
 {
@@ -19,6 +18,78 @@ namespace Ex5
                 }
                 Console.WriteLine();
             }
+
+            int somaRef = 0;
+            for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                somaRef += matriz[0, colunas];
+            Console.WriteLine($"\nSoma de referência = {somaRef}\n");
+
+            //Soma dos elementos de cada linha
+            Console.WriteLine("\nSoma dos elementos de cada linha\n");
+
+            int [] somaLinhas = new int[10];
+            int [] somaColunas = new int[10];
+            bool linhasIguais = true;
+            bool colunasIguais = true;
+            
+
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    somaLinhas[linhas] += matriz[linhas, colunas];
+
+                    if (somaLinhas[linhas] != somaRef)
+                        linhasIguais = false;
+                }
+                Console.WriteLine($"Soma da linha {linhas} = {somaLinhas[linhas]}"); 
+            }
+
+            //Soma dos elementos de cada coluna
+            Console.WriteLine("\nSoma dos elementos de cada coluna\n");
+
+            for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+            {
+                for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+                {
+                    somaColunas[colunas] += matriz[linhas, colunas];
+
+                    if (somaColunas[colunas] != somaRef)
+                        colunasIguais = false;
+                }
+                Console.WriteLine($"Soma da coluna {colunas} = {somaColunas[colunas]}");
+            }
+
+            //Soma dos elementos das diagonal principal
+
+            int somaDiagonal = 0;
+
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    if (linhas == colunas) 
+                    { 
+                    somaDiagonal += matriz[linhas, colunas];
+                    }
+                }
+            }
+            Console.WriteLine($"\nSoma da diagonal principal = {somaDiagonal}");
+
+            //Soma dos elementos das diagonal principal
+
+            int somaDiagSecundaria = 0;
+
+            for (int linhas = 0, colunas = 10 - 1; linhas < matriz.GetLength(0); linhas++, colunas--)
+            {
+                somaDiagSecundaria += matriz[linhas, colunas];
+            }
+            Console.WriteLine($"\nSoma da diagonal secundária = {somaDiagSecundaria}\n");
+
+            if (linhasIguais && colunasIguais && somaDiagSecundaria == somaRef && somaDiagonal == somaRef)
+                Console.WriteLine("É um quadrado mágico!");
+            else
+                Console.WriteLine("Não é um quadrado mágico.");
         }
     }
 }
